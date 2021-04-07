a78b94d68232bc56246f2a179610b312b2ea8c8a
MaxwellSam <81492409+MaxwellSam@users.noreply.github.com>
2021-04-07 10:35:41 +0200

    new commit

--- script1.py
+++ script1.py
@@ -1,9 +1,13 @@
+#! /usr/bin/env python3
+# coding: utf-8
 """"
 BDD 
 03/2021
 Projet Insee
 
+title : script1.py
 authors : MAXWELL - GOMES - SOU
+description : Etape de d'importation et de preparation des données et creation des tables
 """
 
 # Libs 
